105e2bddbc533cd67cff15ea79fb300625e47987
Thanh Pham <126971526+healxer@users.noreply.github.com>
2024-04-07 17:32:28 +0200

    Finish task 1

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
 console.log('DOM');
+
+const commentItem = document.querySelector(
+  '.comments__item.comments__item--newest'
+);
+
+if (commentItem) {
+  const dataInfoItems = commentItem.querySelectorAll('[data-info]');
+
+  console.log(`Znaleziono ${dataInfoItems.length} elementy.`);
+}
